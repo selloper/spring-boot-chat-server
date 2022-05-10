e3419c0433fed114e8e432441458de571608b8bc
Jason Cho <jason@inventis.co.kr>
2022-05-10 08:48:01 -0400

    initial commit

---
+++ src/main/java/com/slw/chat/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.slw.chat.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/chat")
+    public ModelAndView chat() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("chat");
+        return mv;
+    }
+}
